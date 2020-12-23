f849ed60199e4d83e5e468f634c8d4a23c2d161d
romanKrupnov <89263797575@rambler.ru>
2020-12-23 13:55:18 +0300

    1. Подключить middlewares. 2. Создать messageMiddleware и перенести туда логику ответа от робота. 3. Сделать с помощью middleware мигание (подсвечивание в списке на короткое время) чата, в который пришло сообщение от робота. 4. Подключить Router к Redux и перевести переходы между чатами с <Link> на push(). 5. Подключить redux-persist и сохранять с его помощью чаты и сообщения.

---
+++ src/components/Message/Message.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Message extends React.Component{
+    static propTypes = {
+      text: PropTypes.string.isRequired,
+        author: PropTypes.string.isRequired,
+    };
+
+    render() {
+        return (
+            <div className='message'
+            style={{ alignSelf: this.props.author === 'Robot' ? 'flex-start' : 'flex-end', backgroundColor: this.props.author === 'Robot' ? 'lightblue' : 'pink'}}
+            >
+                <div>{this.props.text}</div>
+                 <div className='message-sender'>{this.props.author}</div>
+            </div>
+        );
+    }
+}
---
+++ src/containers/ChatList.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {bindActionCreators} from "redux";
+import connect from 'react-redux/es/connect/connect';
+import {Link} from "react-router-dom";
+import {List, ListItem} from 'material-ui/List';
+import PropTypes from 'prop-types';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import AddIcon from 'material-ui/svg-icons/content/add';
+import {TextField} from "material-ui";
+import {addChat} from "../actions/chatActions";
+import { push } from 'connected-react-router';
+
+class ChatList extends React.Component{
+    static propTypes = {
+        chats: PropTypes.object.isRequired,
+        addChat: PropTypes.func.isRequired,
+        push: PropTypes.func.isRequired,
+    };
+
+    state = {
+      input: '',
+    };
+
+    handleChange = (event) =>{
+        this.setState({[event.target.name]: event.target.value});
+    };
+
+    handleKeyUp = (event) =>{
+      if(event.keyCode === 13){
+          this.handleAddChat();
+      }
+    };
+
+    handleAddChat = () =>{
+        if(this.state.input.length > 0){
+            this.props.addChat(this.state.input);
+            this.setState({input: ''});
+        }
+    };
+    handleNavigate = (link) => {
+        this.props.push(link);
+    };
+
+    render() {
+        const {chats} = this.props;
+        const chatElements = Object.keys(chats).map(chatId =>(
+            <Link to={`/chat/${chatId}`} key={chatId} >
+                <ListItem
+                    primaryText={chats[chatId].title}
+                    leftIcon={<ContentSend/>}
+                    onClick={ () => this.handleNavigate(`/chat/${chatId}`) }
+                />
+            </Link>
+        ));
+        return (
+            <List>
+                {chatElements}
+                <ListItem
+                    key="Add new chat"
+                    leftIcon={ <AddIcon />}
+                    onClick={this.handleAddChat}
+                    style={{height:'60px'}}
+                    children= {<TextField
+                        key="textField"
+                        fullWidth
+                        name="input"
+                        hintText="Добавить новый чат"
+                        onChange={ this.handleChange }
+                        value={ this.state.input }
+                        onKeyUp={ this.handleKeyUp }
+                    />}
+
+                />
+            </List>
+        );
+    }
+}
+
+const mapStateToProps = ({ chatReducer}) =>({
+    chats: chatReducer.chats,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({addChat,push}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChatList);
